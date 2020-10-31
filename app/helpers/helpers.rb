92e48d29dffd58216b72a112cebe2d6be2ed1c1c
Kelin Field <kfield111@hotmail.com>
2020-10-31 04:47:23 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 class Helpers
-
-  def self.current_user (session)
-    session["user_id"]
+  
+  def self.current_user(session_hash)
+    User.find(session_hash["user_id"])
   end
 
   def self.is_logged_in? (session)
