7e8fde1b190ed4c30f7284059d67295788aa52e7
Kelin Field <kfield111@hotmail.com>
2020-10-31 04:22:37 +0000

    Automatically backed up by Learn

--- db/migrate/20201031042114_create_users_table.rb
+++ db/migrate/20201031042114_create_users_table.rb
@@ -1,8 +1,9 @@
 class CreateUsersTable < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-    t.string :username
-    t.string :password
-    t.float :balance
+      t.string :username
+      t.string :password
+      t.float :balance
+    end
   end
 end
